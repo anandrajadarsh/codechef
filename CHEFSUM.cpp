33b75911ebd22e8a5f43ffa38d19bf9b14c42704
anandrajadarsh <54949024+anandrajadarsh@users.noreply.github.com>
2020-10-15 21:06:04 +0530

    Create CHEFSUM.cpp
    Added template

---
+++ CHEFSUM.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(false);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    
+	   
+	}
+	return 0;
+}
