241de878494cd558664fe451d2b9dd6737e18e3e
Adarsh Raj Anand <anandrajadarsh@gmail.com>
2020-10-15 21:06:42 +0530

    Update CHEFSUM.cpp
    Completed code

--- CHEFSUM.cpp
+++ CHEFSUM.cpp
@@ -8,7 +8,30 @@ int main() {
 	cin>>t;
 	while(t--)
 	{
-	    
+	    int n,k;
+	    cin>>n>>k;
+	    int* arr=new int[n];
+	    int greatthanone=0,idx=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>arr[i];
+	    }
+	    sort(arr,arr+n);
+	   
+	   for(int i=0;i<n;i++)
+	   {
+	       if(arr[i]>1)
+	       {
+	           idx=i;
+	           break;
+	       }
+	   }
+	   greatthanone=n-idx;
+	   
+	    if(greatthanone<=k)
+	    cout<<"YES";
+	    else
+	    cout<<"NO";
 	   
 	}
 	return 0;
