137d9c88e052f95fb783e95a008441cc13b5ed0f
anandrajadarsh <54949024+anandrajadarsh@users.noreply.github.com>
2020-10-15 21:03:17 +0530

    Create SPELLBOB.cpp
    Template added

---
+++ SPELLBOB.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+int main(){
+	int t;
+	cout<<"enter test cases";
+	cin>>t;
+	while(t--)
+	{	
+		
+		
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
+
