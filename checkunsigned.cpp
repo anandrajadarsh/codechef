b3824f02b765fd0e4cf93487b27f29333abeb6b3
anandrajadarsh <54949024+anandrajadarsh@users.noreply.github.com>
2020-10-15 21:01:54 +0530

    Create checkunsigned.cpp

---
+++ checkunsigned.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i;
+	cin>>n;
+	unsigned int a[n];
+	for(i=0;i<n;i++)
+	cin>>a[i];
+	cout<<"output is:";
+	for(i=0;i<n;i++)
+	cout<<a[i]<<endl;
+	return 0;
+	
+	
+}
